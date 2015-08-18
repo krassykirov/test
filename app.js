ef0c089c2f068b65f12677989766e70d1d4086da
krassykirov <krassy.kirov@gmail.com>
2015-08-18 15:22:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,54 @@
-var express = require("express");
-var multer = require('multer');
-var app = express();
-var done = false;
+var express=require("express");
+var multer  = require('multer');
+var app=express();
+var done=false;
 var fs = require('fs');
+var path = require('path');
+
 /*Configure the multer.*/
 
-app.use(multer({
-    dest: './Uploads/',
+app.use(multer({ dest: './uploads/',
     rename: function (fieldname, filename) {
-
-        var d = Date.now();
-        var directory = './Uploads' + '/' + d.toString();
-        fs.mkdir(directory, function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('directory : ' + directory + ' created.');
-            }
-        });
-        return filename;
+        return filename+ '-' + Date.now();
     },
     onFileUploadStart: function (file) {
         console.log(file.originalname + ' is starting ...')
     },
     onFileUploadComplete: function (file) {
-        console.log(file.fieldname + ' uploaded to  ' + file.path)
-        done = true;
+        console.log(file.fieldname + ' uploaded to  ' + file.path);
+        var destination = './files';
+        try {
+            var stats = fs.statSync(destination);
+        } catch (e) {
+            fs.mkdirSync(destination);
+        }
+        var matches = file.name.match(/-(\d+)(\.[a-z]+){0,1}/);
+        if (!matches) {
+            throw  new Error('Invalid file name!');
+        }
+        var destDirName = matches[1];
+        fs.mkdirSync(path.join(destination, destDirName));
+
+        var destinationPath = path.join(destination, destDirName, matches[1] + (!matches[2] ? '' : matches[2]));
+        fs.writeFileSync(destinationPath, fs.readFileSync(file.path));
+        fs.unlinkSync(file.path);
+        done=true;
     }
 }));
 
 /*Handling routes.*/
 
-app.get('/', function (req, res) {
+app.get('/',function(req,res){
     res.sendfile("index.html");
 });
 
-app.post('/Uploads', function (req, res) {
-    if (done == true) {
-        console.log(req.files);
+app.post('/api/photo',function(req,res){
+    if(done==true){
         res.end("File uploaded.");
     }
 });
 
 /*Run the server.*/
-app.listen(3000, function () {
+app.listen(3000,function(){
     console.log("Working on port 3000");
 });
-
